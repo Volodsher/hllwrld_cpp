fb5cac3bf00a8914ca1350c650b745d4e5734439
bigriver <volodsher@gmail.com>
2023-05-17 00:18:32 -0400

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by bigriver on 2023-05-17.
+//  Copyright © 2023 bigriver. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
