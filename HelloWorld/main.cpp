1dfcd6b1a6c121c35a97c9bd94cda00ede3dbd20
bigriver <volodsher@gmail.com>
2023-05-18 23:41:21 -0400

    playing with std::cout

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -6,10 +6,25 @@
 //  Copyright © 2023 bigriver. All rights reserved.
 //
 
+// library
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+// this is from original xcode
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+// this is simpler
+//the main function
+int main() {
     // insert code here...
     std::cout << "Hello, World!\n";
-    return 0;
+    // character output stream: “see-out”
+    std::cout << "       1\n";
+    std::cout << "     2 3\n";
+    std::cout << "   4 5 6\n";
+    std::cout << "7 8 9 10\n";
+    
 }
