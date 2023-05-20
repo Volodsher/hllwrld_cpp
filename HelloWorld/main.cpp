d45dd234445d200733f9c043490b2dfc682c8c7b
bigriver <volodsher@gmail.com>
2023-05-20 19:25:47 -0400

    switch statement

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -21,23 +21,65 @@
 
 
 
-// library
-#include <iostream>
-
-// this is from original xcode
-//int main(int argc, const char * argv[]) {
-//    // insert code here...
-//    std::cout << "Hello, World!\n";
-//    return 0;
+//// library
+//#include <iostream>
+//
+//// this is from original xcode
+////int main(int argc, const char * argv[]) {
+////    // insert code here...
+////    std::cout << "Hello, World!\n";
+////    return 0;
+////}
+//
+//// this is simpler
+////the main function
+//int main() {
+//
+//    int tip = 0;
+//
+//    std::cout << "Enter tip amount: ";
+//    std::cin >> tip;
+//    std::cout << "You paid " << tip << " dollars." << "\n";
 //}
 
-// this is simpler
-//the main function
+#include <iostream>
+
 int main() {
     
-    int tip = 0;
-
-    std::cout << "Enter tip amount: ";
-    std::cin >> tip;
-    std::cout << "You paid " << tip << " dollars." << "\n";
+    int weight;
+    std::cout << "Enter your earth weight:";
+    std::cin >> weight;
+    
+    int planet;
+    std::cout << "Enter a number for the planet you wants to fight on:";
+    std::cin >> planet;
+    
+    double weightOnPlanet = 0.0;
+    
+    switch (planet) {
+        case 1:
+            weightOnPlanet = weight * 0.38;
+            break;
+        case 2:
+            weightOnPlanet = weight * 0.91;
+            break;
+        case 3:
+            weightOnPlanet = weight * 0.38;
+            break;
+        case 4:
+            weightOnPlanet = weight * 2.34;
+            break;
+        case 5:
+            weightOnPlanet = weight * 1.06;
+            break;
+        case 6:
+            weightOnPlanet = weight * 0.92;
+            break;
+        case 7:
+            weightOnPlanet = weight * 1.19;
+            break;
+    }
+    
+    std::cout << weightOnPlanet << "\n";
+    return weightOnPlanet;
 }
